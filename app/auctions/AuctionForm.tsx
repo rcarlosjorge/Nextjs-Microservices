418dd2729d74f41b9e9411d4fac61f41b6149802
rcarlosjorge <carlosepjorge@gmail.com>
2024-05-17 19:48:03 -0400

    React hook form added

---
+++ app/auctions/AuctionForm.tsx
@@ -0,0 +1,52 @@
+"use client";
+import { Button, TextInput } from "flowbite-react";
+import React from "react";
+import { useForm, SubmitHandler, FieldValues } from "react-hook-form";
+
+export default function AuctionForm() {
+  const {
+    register,
+    handleSubmit,
+    setFocus,
+    formState: { isSubmitting, isValid, isDirty, errors },
+  } = useForm();
+
+  function onSubmit(data: FieldValues) {
+    console.log(data);
+  }
+
+  return (
+    <form className="flex flex-col mt-3" onSubmit={handleSubmit(onSubmit)}>
+      <div className="mb-3 block">
+        <TextInput
+          {...register("make", { required: "make is required" })}
+          placeholder="Make"
+          color={errors?.make && "failure"}
+          helperText={errors.make?.message as string}
+        />
+      </div>
+      <div className="mb-3 block">
+        <TextInput
+          {...register("model", { required: "model is required" })}
+          placeholder="model"
+          color={errors?.model && "failure"}
+          helperText={errors.model?.message as string}
+        />
+      </div>
+      <div className="flex justify-between">
+        <Button outline color="gray">
+          Cancel
+        </Button>
+        <Button
+          isProcessing={isSubmitting}
+        //   disabled={!isValid}
+          type="submit"
+          outline
+          color="success"
+        >
+          Submit
+        </Button>
+      </div>
+    </form>
+  );
+}
