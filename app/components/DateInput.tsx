4129e72d74f691d2862118abb9da694470a69623
rcarlosjorge <carlosepjorge@gmail.com>
2024-05-30 11:58:29 -0400

    Bugfix some warnings

--- app/components/DateInput.tsx
+++ app/components/DateInput.tsx
@@ -1,7 +1,6 @@
 import { Label, TextInput } from "flowbite-react";
 import React from "react";
 import { useController, UseControllerProps } from "react-hook-form";
-import "react-datepicker/dist/react-datepicker.css";
 import DatePicker, { ReactDatePickerProps } from "react-datepicker";
 
 type Props = {
--- app/providers/SignalRProvider.tsx
+++ app/providers/SignalRProvider.tsx
@@ -20,20 +20,21 @@ export default function SignalRProvider({ children, user }: Props) {
   const [connection, setConnection] = useState<HubConnection | null>(null);
   const setCurrentPrice = useAuctionStore((state) => state.setCurrentPrice);
   const addBid = useBidStore((state) => state.addBid);
+  const apiUrl =
+    process.env.NODE_ENV === "production"
+      ? "https://api.carsties.com/notifications"
+      : process.env.NEXT_PUBLIC_NOTIFY_URL;
 
   useEffect(() => {
-    const notifyUrl = process.env.NEXT_PUBLIC_NOTIFY_URL;
-    if (notifyUrl) {
+    if (apiUrl) {
       const newConnection = new HubConnectionBuilder()
-        .withUrl(notifyUrl)
+        .withUrl(apiUrl!)
         .withAutomaticReconnect()
         .build();
 
       setConnection(newConnection);
-    } else {
-      console.error("NEXT_PUBLIC_NOTIFY_URL is not defined");
     }
-  }, []);
+  }, [apiUrl]);
 
   useEffect(() => {
     if (connection) {
